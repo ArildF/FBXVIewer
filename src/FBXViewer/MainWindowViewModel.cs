5fcc5ffa31ac5b40b3225d72cdd707d669e148c3
Arild Fines <arild.fines@broadpark.no>
2020-05-24 01:47:14 +0200

    Ensure you can only have one file open (resolves some issues)

--- src/FBXViewer/MainWindowViewModel.cs
+++ src/FBXViewer/MainWindowViewModel.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.ObjectModel;
 using System.Reactive.Linq;
-using Microsoft.Win32;
 using ReactiveUI;
 
 namespace FBXViewer
@@ -52,6 +51,7 @@ namespace FBXViewer
         {
             try
             {
+                RootNodes.Clear();
                 var fileNode = _fileFactory();
                 fileNode.Load(fileName);
                 RootNodes.Add(_nodeFactory(fileNode));
