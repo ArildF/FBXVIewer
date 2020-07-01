f063ddafb647b417ea544fdcf4b4611aa2c45b04
Arild Fines <arild.fines@broadpark.no>
2020-07-01 18:52:08 +0200

    Nullable, this is.

--- src/FBXViewer/CommandLineOptions.cs
+++ src/FBXViewer/CommandLineOptions.cs
@@ -10,7 +10,7 @@ namespace FBXViewer
     public class CommandLineOptions
     {
         [Value(0, Required = false)]
-        public string FileName { get; set; }
+        public string? FileName { get; set; }
         
         [Option(Required = false, Default = (Renderer)Renderer.OpenGL)]
         public Renderer Renderer { get; set; }
